ca8b1e16aeaf08fad9f40b2e51320989f64a3b11
igor-a-vm <igor-a-vm@yandex.ru>
2023-12-04 15:49:39 +0300

    add init key

--- src/DB.ts
+++ src/DB.ts
@@ -1,54 +1,61 @@
-import { DataSource, DataSourceOptions, TreeChildren } from "typeorm";
-import { Auth } from "./entity/Auth";
-import { Chat } from "./entity/Chat";
-import { Contact } from "./entity/Contact";
-import { GroupMetadata } from "./entity/GroupMetadata";
-import { Message } from "./entity/Message";
-import { MessageDic } from "./entity/MessageDic";
-import { Presence } from "./entity/Presence";
-import { PresenceDic } from "./entity/PresenceDic";
+import { DataSource, DataSourceOptions, TreeChildren } from 'typeorm'
+import { Auth } from './entity/Auth'
+import { Chat } from './entity/Chat'
+import { Contact } from './entity/Contact'
+import { GroupMetadata } from './entity/GroupMetadata'
+import { Message } from './entity/Message'
+import { MessageDic } from './entity/MessageDic'
+import { Presence } from './entity/Presence'
+import { PresenceDic } from './entity/PresenceDic'
 
 class DB {
-  static instance = new DB();
-  private dataSource: DataSource;
-  private constructor() {}
+    static instance = new DB()
+    private dataSource: DataSource
+    private constructor() {}
 
-  get = async (
-    db: DataSourceOptions,
-    options?: {
-      debug?: boolean;
-      sync?: boolean;
-    }
-  ) => {
-    this.dataSource =
-      !options?.sync && this.dataSource
-        ? this.dataSource
-        : await new DataSource({
-            synchronize: options?.sync,
-            migrations: [],
-            logging: options?.debug,
-            charset: "cp1251_general_ci",
-            ...db,
-            entities: [
-              Auth,
-              Chat,
-              Contact,
-              GroupMetadata,
-              MessageDic,
-              Message,
-              PresenceDic,
-              Presence,
-              ...((db.entities as any) || []),
-            ],
-          } as any).initialize();
+    get = async (
+        db: DataSourceOptions,
+        options?: {
+            debug?: boolean
+            sync?: boolean
+            key?: string
+        }
+    ) => {
+        this.dataSource =
+            !options?.sync && this.dataSource
+                ? this.dataSource
+                : await new DataSource({
+                      synchronize: options?.sync,
+                      migrations: [],
+                      logging: options?.debug,
+                      charset: 'cp1251_general_ci',
+                      ...db,
+                      entities: [
+                          Auth,
+                          Chat,
+                          Contact,
+                          GroupMetadata,
+                          MessageDic,
+                          Message,
+                          PresenceDic,
+                          Presence,
+                          ...((db.entities as any) || []),
+                      ],
+                  } as any).initialize()
 
-    try {
-      await this.dataSource.getRepository(Auth).find();
-    } catch {
-      return await this.get(db, { ...options, sync: true });
+        try {
+            if (options && options.key) {
+                await this.dataSource
+                    .getRepository(Auth)
+                    .find({ where: { key: options.key } })
+                return
+            }
+            await this.dataSource.getRepository(Auth).find()
+        } catch {
+            return this.get(db, { ...options, sync: true })
+        }
+        return this.dataSource
     }
-    return this.dataSource;
-  };
 }
 
-export default DB.instance;
+export default DB.instance
--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,53 @@
-import { DataSource, DataSourceOptions } from "typeorm";
-import AuthHandle from "./bottle/AuthHandle";
-import StoreHandle, { StoreHandleOptions } from "./bottle/StoreHandle";
-import DB from "./DB";
-import { Auth } from "./entity/Auth";
+import { DataSource, DataSourceOptions } from 'typeorm'
+import AuthHandle from './bottle/AuthHandle'
+import StoreHandle, { StoreHandleOptions } from './bottle/StoreHandle'
+import DB from './DB'
+import { Auth } from './entity/Auth'
 class BaileysBottle {
-  static instance = new BaileysBottle();
-  private constructor() {}
+    static instance = new BaileysBottle()
+    private constructor() {}
 
-  private createStore = async (
-    ds: DataSource,
-    storeName: string,
-    options?: StoreHandleOptions
-  ) => {
-    var store = await ds.getRepository(Auth).findOne({
-      where: { key: storeName },
-    });
-    if (!store)
-      store = await ds.getRepository(Auth).save({
-        key: storeName,
-        value: "",
-        chats: [],
-        contacts: [],
-        groups: [],
-        messageDics: [],
-        presenceDics: [],
-      });
-    return {
-      auth: new AuthHandle(ds, storeName),
-      store: new StoreHandle(ds, store, options),
-      _ds: ds,
-    };
-  };
-
-  init = async (
-    db: DataSourceOptions,
-    options?: {
-      debug?: boolean;
-      sync?: boolean;
+    private createStore = async (
+        ds: DataSource,
+        storeName: string,
+        options?: StoreHandleOptions
+    ) => {
+        var store = await ds.getRepository(Auth).findOne({
+            where: { key: storeName },
+        })
+        if (!store)
+            store = await ds.getRepository(Auth).save({
+                key: storeName,
+                value: '',
+                chats: [],
+                contacts: [],
+                groups: [],
+                messageDics: [],
+                presenceDics: [],
+            })
+        return {
+            auth: new AuthHandle(ds, storeName),
+            store: new StoreHandle(ds, store, options),
+            _ds: ds,
+        }
     }
-  ): Promise<{
-    createStore: (
-      storeName?: string,
-      storeOptions?: StoreHandleOptions
-    ) => Promise<{ auth: AuthHandle; store: StoreHandle; _ds: DataSource }>;
-  }> => ({
-    createStore: async (...args: any[]) =>
-      this.createStore.apply(null, [await DB.get(db, options), ...args]),
-  });
+
+    init = async (
+        db: DataSourceOptions,
+        options?: {
+            debug?: boolean
+            sync?: boolean
+            key?: string
+        }
+    ): Promise<{
+        createStore: (
+            storeName?: string,
+            storeOptions?: StoreHandleOptions
+        ) => Promise<{ auth: AuthHandle; store: StoreHandle; _ds: DataSource }>
+    }> => ({
+        createStore: async (...args: any[]) =>
+            this.createStore.apply(null, [await DB.get(db, options), ...args]),
+    })
 }
 
-export default BaileysBottle.instance;
+export default BaileysBottle.instance
